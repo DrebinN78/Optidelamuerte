c3bd6e323e59a6b083fd77ac0782f54c091a8863
Dorian Gélas <dorian.gelas@gmail.com>
2021-03-11 18:28:07 +0100

    reworked input polling loop

--- ConsoleApplication1/Game.cpp
+++ ConsoleApplication1/Game.cpp
@@ -10,99 +10,104 @@ Game::~Game() = default;
 
 bool Game::Init(SDL_Renderer * screenRenderer)
 {
-    _modele = 1;
-    _couleur = "blanc";
-    _isRunning = true;
-    _screenRenderer = screenRenderer;
-    srand(time(NULL));
+	_modele = 1;
+	_couleur = "blanc";
+	_isRunning = true;
+	_screenRenderer = screenRenderer;
+	srand(time(NULL));
 	return true;
 }
 
 void Game::Update(int deltaTime)
 {
-    SDL_Event e;
-    while (SDL_PollEvent(&e) != 0)
-    {
-        if (e.type == SDL_QUIT)
-        {
-            _isRunning = false;
-        }
-        else if (e.type == SDL_KEYDOWN)
-        {
-            if(e.key.keysym.sym == SDLK_ESCAPE)
-            {
-                _isRunning = false;
-            }
-            if (e.key.keysym.sym == SDLK_1)
-            {
-                _couleur = "blanc";
-            }
-            if (e.key.keysym.sym == SDLK_2)
-            {
-                _couleur = "rouge";
-            }
-            if (e.key.keysym.sym == SDLK_3)
-            {
-                _couleur = "vert";
-            }
-            if (e.key.keysym.sym == SDLK_4)
-            {
-                _couleur = "bleu";
-            }
-            if (e.key.keysym.sym == SDLK_9)
-            {
-                _modele = 1;
-            }
-            if (e.key.keysym.sym == SDLK_0)
-            {
-                _modele = 2;
-            }
-        }
-        else if (e.type == SDL_MOUSEBUTTONDOWN)
-        {
-            if (e.button.button == 1)
-            {
-                int mouseX, mouseY;
-                SDL_GetMouseState(&mouseX, &mouseY);
-                CreerGenerateurParticule(mouseX, mouseY);
-            }
-        }
-    }
+	SDL_Event e;
+	while (SDL_PollEvent(&e) != 0)
+	{
+		if (e.type == SDL_QUIT)
+		{
+			_isRunning = false;
+		}
+		else if (e.type == SDL_KEYDOWN)
+		{
+			switch (e.key.keysym.sym)
+			{
+			case SDLK_ESCAPE: {
+				_isRunning = false;
+			}
+							break;
+			case SDLK_1: {
+				_couleur = "blanc";
+			}
+					   break;
+			case SDLK_2: {
+				_couleur = "rouge";
+			}
+					   break;
+			case SDLK_3: {
+				_couleur = "vert";
+			}
+					   break;
+			case SDLK_4: {
+				_couleur = "bleu";
+			}
+					   break;
+			case SDLK_9: {
+				_modele = 1;
+			}
+					   break;
+			case SDLK_0: {
+				_modele = 2;
+			}
+					   break;
+			default:
+				break;
+			}
+		}
+		else if (e.type == SDL_MOUSEBUTTONDOWN)
+		{
+			if (e.button.button == 1)
+			{
+				int mouseX, mouseY;
+				SDL_GetMouseState(&mouseX, &mouseY);
+				CreerGenerateurParticule(mouseX, mouseY);
+			}
+		}
+	}
 
-    if (_isRunning)
-    {
-        for(int i = 0; i < _generateurs.size(); i++)
-        {
-            _generateurs[i]->Update(deltaTime);
-            if (_generateurs[i]->EstActif() == false)
-            {
-                auto generateur = _generateurs[i];
-                _generateurs.erase(std::find(_generateurs.begin(), _generateurs.end(), generateur));
-                delete generateur;
-            }
-        }
-    }
+	if (_isRunning)
+	{
+		for (int i = 0; i < _generateurs.size(); i++)
+		{
+			_generateurs[i]->Update(deltaTime);
+			if (_generateurs[i]->EstActif() == false)
+			{
+				auto generateur = _generateurs[i];
+				_generateurs.erase(std::find(_generateurs.begin(), _generateurs.end(), generateur));
+				delete generateur;
+			}
+		}
+	}
 }
 
-void Game::Render(SDL_Renderer* screenRenderer)
+void Game::Render(SDL_Renderer * screenRenderer)
 {
-    SDL_RenderClear(screenRenderer);
+	SDL_RenderClear(screenRenderer);
 	if (_isRunning)
-    {
-        for (int i = 0; i < _generateurs.size(); i++)
-        {
-            _generateurs[i]->Render(screenRenderer);
-        }
-    }
+	{
+		for (int i = 0; i < _generateurs.size(); i++)
+		{
+			_generateurs[i]->Render(screenRenderer);
+		}
+	}
 }
 
 void Game::Release()
 {
-    for (int i = 0; i < _generateurs.size(); i++)
-    {
-        delete _generateurs[i];
-    }
-    _generateurs.clear();
+	for (int i = 0; i < _generateurs.size(); i++)
+	{
+		delete _generateurs[i];
+	}
+	_generateurs.clear();
 }
 
 
@@ -113,13 +118,13 @@ bool Game::IsRunning()
 
 void Game::CreerGenerateurParticule(int posX, int posY)
 {
-    if (_generateurs.size() == 5)
-    {
-        delete _generateurs[0];
-        _generateurs.erase(_generateurs.begin());
-    }
-    Vector _position(posX, posY);
-    auto generateur = new GenerateurParticule();
-    generateur->Init(_screenRenderer, rand() % 20, 20 + rand() % 80, 500 + rand() % 2500, "particle" + std::to_string(_modele), _couleur, rand() % 5, rand() % 15, _position, 16, 64, 100 + rand() % 500, rand() % 90);
-    _generateurs.push_back(generateur);
+	if (_generateurs.size() == 5)
+	{
+		delete _generateurs[0];
+		_generateurs.erase(_generateurs.begin());
+	}
+	Vector _position(posX, posY);
+	auto generateur = new GenerateurParticule();
+	generateur->Init(_screenRenderer, rand() % 20, 20 + rand() % 80, 500 + rand() % 2500, "particle" + std::to_string(_modele), _couleur, rand() % 5, rand() % 15, _position, 16, 64, 100 + rand() % 500, rand() % 90);
+	_generateurs.push_back(generateur);
 }
